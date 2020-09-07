6cfabb571d6c042e88d884ef1dfb150760451fd5
aticbtic <angela.i.tan@gmail.com>
2020-09-07 04:23:08 +0000

    Added edit and update support

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     
+    def edit
+        @article = Article.find(params[:id])
+    end
+    
     def create
         @article = Article.new(params.require(:article).permit(:title, :description))   
 #        render plain:@article.inspect
@@ -19,6 +23,17 @@ class ArticlesController < ApplicationController
             redirect_to article_path(@article)
         else
             render 'new'
+        end
+    end
+    
+    def update
+        @article = Article.find(params[:id])
+        #require statement is to whitelist was is acceptable parameters for the patch request
+        if @article.update(params.require(:article).permit(:title, :description))
+          flash[:notice] = "Article was updated successfully."
+          redirect_to @article
+        else
+          render 'edit'
         end
     end
 end
