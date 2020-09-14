64ea05cca954d060cc6c39e4af5214c837b792b4
aticbtic <angela.i.tan@gmail.com>
2020-09-14 02:49:59 +0000

    Added Bootstrap and refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,39 +1,52 @@
 class ArticlesController < ApplicationController
-    def show
-        @article = Article.find(params[:id])
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def index
+    @articles = Article.all
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+    else
+      render 'new'
     end
-    
-    def index
-        @article = Article.all
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
     end
-    
-    def new
-        @article = Article.new
-    end
-    
-    def edit
-        @article = Article.find(params[:id])
-    end
-    
-    def create
-        @article = Article.new(params.require(:article).permit(:title, :description))   
-#        render plain:@article.inspect
-        if @article.save 
-            flash[:notice] = "Article was saved succesfully"
-            redirect_to article_path(@article)
-        else
-            render 'new'
-        end
-    end
-    
-    def update
-        @article = Article.find(params[:id])
-        #require statement is to whitelist was is acceptable parameters for the patch request
-        if @article.update(params.require(:article).permit(:title, :description))
-          flash[:notice] = "Article was updated successfully."
-          redirect_to @article
-        else
-          render 'edit'
-        end
-    end
+  end
+
+  def destroy
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
 end
